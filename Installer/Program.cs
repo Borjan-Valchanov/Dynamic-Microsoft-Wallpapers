805efb23204a9df05b6eece4bd6aaafa2aedc0e2
user <git@bvch.mx>
2023-07-27 12:27:19 +0200

    Add project files.

---
+++ Installer/Program.cs
@@ -0,0 +1,9 @@
+namespace Installer {
+	internal static class Program {
+		[STAThread]
+		static void Main() {
+			ApplicationConfiguration.Initialize();
+			// Application.Run(new Form1());
+		}
+	}
+}
---
+++ WallpaperTray/Program.cs
@@ -0,0 +1,9 @@
+namespace WallpaperTray {
+	internal static class Program {
+		[STAThread]
+		static void Main() {
+			ApplicationConfiguration.Initialize();
+			// Application.Run(new Form1());
+		}
+	}
+}

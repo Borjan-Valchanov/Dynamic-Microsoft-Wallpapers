8999382776ab9d2738a4d407e1764feb5bd07db9
Borjan-Valchanov <git@bvch.mx>
2023-07-28 18:59:13 +0200

    Added Download class Implemented ImageCrawler.Downloader.UpdateIndex Added list of current downloads to ImageCrawler.Downloader

---
+++ ImageCrawler/Download.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCrawler {
+	internal class Download {
+		// TODO
+		public Download() {
+			throw new NotImplementedException();
+		}
+		public void Abort() {
+			throw new NotImplementedException();
+		}
+	}
+}
--- ImageCrawler/Downloader.cs
+++ ImageCrawler/Downloader.cs
@@ -7,9 +7,16 @@ using System.Threading.Tasks;
 namespace ImageCrawler {
 	// Use this class to download a specified number of images simultaneously to a specified directory from a specified index.
 	internal class Downloader {
+		// Holds to be downloaded elements
+		Queue<string> index;
+		// Holds ongoing downloads
+		List<Download> downloading;
 		// TODO
 		public Downloader() {
 			throw new NotImplementedException();
+		}
+		public void UpdateIndex(Queue<string> _index) {
+			index = _index;
 		}
 	}
 }

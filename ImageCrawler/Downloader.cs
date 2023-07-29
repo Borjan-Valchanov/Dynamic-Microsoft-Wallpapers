3151ff247ff5369ca8dbb91f7bff3556f8b1b36d
Borjan-Valchanov <git@bvch.mx>
2023-07-29 09:27:22 +0200

    Added a download destination directory field to ImageCrawler.Downloader Added accessors to ImageCrawler.Downloader.parallelDownloadMax to check if new downloads can be started after the limit is raised Running downloads list is now initialised in ImageCrawler.Downloader's constructor Added comment explaining ImageCrawler.Downloader.UpdateIndex() Added ImageCrawler.Downloader.startNewDownloads to check whether new downloads can be started and if so, start them.

--- ImageCrawler/Downloader.cs
+++ ImageCrawler/Downloader.cs
@@ -7,19 +7,44 @@ using System.Threading.Tasks;
 namespace ImageCrawler {
 	// Use this class to download a specified number of images simultaneously to a specified directory from a specified index.
 	internal class Downloader {
+		// Specifies download destination directory
+		string fileDestDir;
 		// Holds to be downloaded elements
 		Queue<string> index;
 		// Holds ongoing downloads
 		List<Task> downloading;
 		// Maximum amount of downloads in parallel
-		public int parallelDownloadMax;
-		// TODO
-		public Downloader(Queue<string> _index, int _parallelDownloadMax = 1) {
+		public int parallelDownloadMax {
+			get {
+				return parallelDownloadMax;
+			}
+			set {
+				// if the maximum download limit is raised, we can start new downloads
+				bool initNewDownloads = value > parallelDownloadMax;
+				parallelDownloadMax = value;
+				if (initNewDownloads) startNewDownloads();
+			}
+		}
+		public Downloader(string _fileDestDir, Queue<string> _index, int _parallelDownloadMax = 1) {
+			fileDestDir = _fileDestDir;
 			index = _index;
 			parallelDownloadMax = _parallelDownloadMax;
+			downloading = new List<Task>();
+			startNewDownloads();
 		}
+		// Intercept when the ImageCrawlerSvc object makes a change to the Downloader's download index,
+		// so new downloads can be initiated right away.
+		// This is used instead of an accessor so index changes from within the downloader do not cause a
+		// startNewDownloads() call
 		public void UpdateIndex(Queue<string> _index) {
 			index = _index;
+			startNewDownloads();
+		}
+		// Start new downloads.
+		// Will be called when a download finishes, the download file index updates, or the parallel download limit is raised
+		private void startNewDownloads() {
+			if (index.Count == 0) return;
+			// TODO
 		}
 	}
 }
